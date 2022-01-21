406b14ef9c19a1ddc2e038214835cbce3c7e4896
ozzaynetworks <ozzaynetworks@gmail.com>
2022-01-21 15:04:56 +0300

    kelvin: the gant chart

--- main/js/chart-data/project-timelines.js
+++ main/js/chart-data/project-timelines.js
@@ -63,6 +63,45 @@ Highcharts.ganttChart('project-timelines', {
         // selected: 0
     },
 
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            grouping: false,
+            pointPadding: 0.2,
+            dataLabels: {
+                enabled: false
+            },
+            states: {
+                hover: {
+                    enabled: false
+                }
+            },
+            //            point: {
+            //                events: {
+            //                    mouseOver: updateStackColor(0.2),
+            //                    mouseOut: updateStackColor(0)
+            //                }
+            //            }
+
+        },
+        series: {
+            //connectNulls: true
+
+
+
+            cursor: 'pointer',
+            point: {
+                events: {
+                    click: function() {
+                        location.href = this.options.url;
+                    }
+                }
+            }
+
+
+        },
+    },
+
     series: [{
         name: 'Project Timelines',
         data: [{
@@ -70,16 +109,20 @@ Highcharts.ganttChart('project-timelines', {
             end: Date.UTC(2018, 1, 2),
             completed: 0.95,
             name: 'Establishing Instant Coffee Plant In Uganda',
+            url: 'COFFEE.html',
+
             completed: {
                 amount: 0.95,
                 fill: '#222f5f'
             },
+
 
         }, {
             start: Date.UTC(2008, 1, 2),
             end: Date.UTC(2012, 11, 5),
             completed: 0.5,
             name: 'Luweero Fruit Processing Project',
+            url: 'fruit-processing.html',
             completed: {
                 amount: 0.5,
                 fill: '#222f5f'
@@ -89,6 +132,7 @@ Highcharts.ganttChart('project-timelines', {
             end: Date.UTC(2021, 11, 9),
             completed: 0.15,
             name: 'Maize Milling Project',
+            url: 'Maize-Milling-Project-details-gulu.html',
             completed: {
                 amount: 0.15,
                 fill: '#222f5f'
@@ -100,12 +144,14 @@ Highcharts.ganttChart('project-timelines', {
                 amount: 0.3,
                 fill: '#222f5f'
             },
-            name: 'Multi Fruit Juice Factory'
+            name: 'Multi Fruit Juice Factory',
+            url: 'fruit-processing.html',
         }, {
             start: Date.UTC(2009, 11, 10),
             end: Date.UTC(2013, 11, 23),
             completed: 0.53,
             name: 'Setting Up A Modern Slaughterhouse In Uganda ',
+            url: '#',
             completed: {
                 amount: 0.53,
                 fill: '#222f5f'
@@ -115,6 +161,7 @@ Highcharts.ganttChart('project-timelines', {
             end: Date.UTC(2015, 11, 25, 16),
             completed: 0.73,
             name: 'Pearl Rice Limited',
+            url: 'rice.html',
             completed: {
                 amount: 0.73,
                 fill: '#222f5f'
@@ -124,6 +171,7 @@ Highcharts.ganttChart('project-timelines', {
             end: Date.UTC(2015, 11, 25, 16),
             completed: 0.73,
             name: 'Dairy Value Chain Enhancement for Increased Market Access',
+            url: '#',
             completed: {
                 amount: 0.73,
                 fill: '#222f5f'
@@ -133,6 +181,7 @@ Highcharts.ganttChart('project-timelines', {
             end: Date.UTC(2021, 11, 25, 16),
             completed: 0.32,
             name: 'Youth Empowerment through Dairy Value Addition',
+            url: '#',
             completed: {
                 amount: 0.32,
                 fill: '#222f5f'
